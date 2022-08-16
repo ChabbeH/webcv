220d09036fb3515ecf28c7369a184b030aae4e3e
ChabbeH <72022949+ChabbeH@users.noreply.github.com>
2022-08-16 14:59:27 +0200

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Cv from "./Cv";
 import HeaderSocials from "./HeaderSocials";
-import ME from "../../assets/me.png";
 import styled from "styled-components";
 import { mobile } from "../../responsive";
 
@@ -14,10 +13,6 @@ function Header() {
         <h5 className="text-light">Frontend Developer</h5>
         <Cv />
         <HeaderSocials />
-
-        {/*  <ImgContainer>
-          <img src={ME} alt="me" />
-        </ImgContainer> */}
 
         <ScrollDown href="#contact">Skrolla Ner</ScrollDown>
       </Container>
@@ -38,18 +33,6 @@ const Container = styled.div`
   text-align: center;
   height: 50%;
   position: relative;
-`;
-
-const ImgContainer = styled.div`
-  background: linear-gradient(var(--color-primary), transparent);
-  width: 22rem;
-  height: 30rem;
-  position: absolute;
-  left: calc(50% - 11rem);
-  margin-top: 4rem;
-  border-radius: 8rem 8rem 0 0;
-  overflow: hidden;
-  padding: 5rem 1.5rem 1.5rem 1, 5rem;
 `;
 
 const ScrollDown = styled.a`
